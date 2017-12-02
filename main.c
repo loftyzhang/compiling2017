f123acb599a495ac4bfbc3a712665be98f58ee51
loftyzhang <457337011@qq.com>
2017-12-02 10:30:04 +0800

    继续语法分析
    已经基本将程序确定为符号表+虚拟地址空间+运行栈的模式，生成pcode后用类似汇编程序的形式进行解释执行，此次代码中尚未考虑到分程序主体部分的begin和en标记出的复合语句，需要在后面加进去。

--- main.c
+++ main.c
@@ -8,21 +8,27 @@
 #include <math.h>
 
 #define norw 21//number of reserved words
+FILE* fin;
 
 enum code{LIT,OPR,LOD,STO,CAL,CLL,ADD,JMP,JPC,RED,WRT}///这里add是数据栈顶指针增加a,CAL调用函数，CLL调用过程
+code codes[1000]={RED};//pcode指令的指令部分
 
-code codes[1000]={RED};
-
-
-
-FILE* fin;
+int operand[1000][2]={{0},{0}};//pcode指令的操作数部分
 int err = 0;//number of errors
 int num_t = 0;//读取的token数量
 int num_l = 0;//行数
 int num_i = 0;//符号表项数
+int addr = 0;///虚拟地址空间中的地址
+int depth = 0;///调用层次数
+int top = 0;///运行栈栈顶
+int bp = 0;///当前分程序数据区的起始地址
+int p0 = 0;///解释执行的pcode下标
+int p1 = 0;///解释执行的下一条pcode
+
+float vm[1000] = {0};//模拟的地址空间-addr
+float run_stack[100]={0};///运行栈-top bp p0 p1
 
 char curc;//当前字符current char
-
 char* reserved[norw];//
 char* typeof_sym[] = {
 	"ident",    "rword",//标识符、保留字
@@ -37,19 +43,32 @@ char* typeof_sym[] = {
 };///symbol的类型，主要是为了第一次作业服务
 
 typedef struct sym{
-	char name[100];
-	char type[20];
-	float value;
+	char name[100];///名称，也可以保存一个完整的字符串
+	char type[20];//类型
+	float value;//值
+    int level;///声明层次
+    int addr;///这里对过程和函数是起始地址，对其内部声明的量是相对地址。
 }symbol;//这里只记录symbol的名称和类型，若为变量或常量则以浮点数形式记录其值，字符保存的是ascii码
-//对于function 和procedure,value 保存其入口
+//对于function 和procedure,value 保存其入口地址
 
 symbol token0;//当前的token
-symbol symbols[100];
+symbol syms[100];///符号表
+
 
 void error(int a ,int b);
 code listcode();
-void statement();
-
+int statement();
+void const_dec(symbol sym);
+void var_dec(symbol sym);
+void pro_dec(symbol sym);
+void func_dec(symbol sym);
+void pro_call(symbol sym);
+void func_call(symbol sym);
+void reading();
+void writing();
+void if_state();
+void for_state();
+void while_state();
 symbol get_sym();
 int position(symbol sym);
 int search_rword(char* s);///确认sym是否是保留字，若是则返回其标号，不是则返回-1
@@ -80,7 +99,7 @@ code listcode(){
 
 }
 
-void statement(symbol sym){
+int statement(symbol sym){
     int i = 0;
     symbol token;
     if(strcmp(sym.type,"ident")==0){
@@ -89,7 +108,7 @@ void statement(symbol sym){
             token = get_sym;
             if(strcmp(token.type,"semicolon")==0){///赋值语句肯定是分号结尾没跑了
                 printf("this is a assignment statement!\n");
-                return ;
+                return 0;
             }////这里还需要判断是否为函数或过程的调用语句
         }
     }
@@ -97,63 +116,67 @@ void statement(symbol sym){
         token = get_sym();
         const_dec(token);
         printf("this is a const declaration statement!\n");
-        return;
+        return 0;
     }
     else if(strcmp(sym.name,"var")==0){
         token = get_sym();
         var_dec(token);
         printf("this is a var declaration statement!\n");
-        return;
+        return 0;
     }
     else if(strcmp(sym.name,"procedure")==0){
         token = get_sym;
         pro_dec(token);
         printf("this is a procedure declaration statement!\n");
-        return;
+        return 0;
     }
     else if(strcmp(sym.name,"function")==0){
         token = get_sym();
         func_dec(token);
         printf("this is a function declaration statement!\n");
-        return;
+        return 0;
     }/////根据getsym函数的特性，先考虑保留字的问题，再故这四个分支是先判断声明再判断调用
     else if(strcmp(sym.type,"procedure")==0){
         token = sym;
         pro_call(token);
         printf("this is a procedure call statement!\n");
-        return;
+        return 0;
     }
     else if(strcmp(sym.type,"function")==0){
         token = sym;
         func_call(token);
         printf("this is a function call statement!\n");
-        return;
+        return 0;
     }
     else if(strcmp(sym.name,"read")==0){
         reading();
         printf("this is a read statement!\n");
-        return;
+        return 0;
     }
     else if(strcmp(sym.name,"write")==0){
         writing();
         printf("this is a read statement!\n");
-        return;
+        return 0;
     }
     else if(strcmp(sym.name,"if")==0){
         if_state();
         printf("this is a if statement!\n");///应该顺便解决else和then分支
-        return;
+        return 0;
     }
     else if(strcmp(sym.name,"while")==0){
         while_state();
         printf("this is a while statement!\n");
-        return;
+        return 0;
     }
     else if(strcmp(sym.name,"for")==0){
         for_state();
         printf("this is a for statement!\n");
-        return;
+        return 0;
     }
+    else if(strcmp(sym.name,"begin")==0){
+        return 0;
+    }
+    return 1;
 }/* read  
     write
     const_dec
@@ -174,10 +197,51 @@ void statement(symbol sym){
 */
 
 void const_dec(symbol sym){
-
+    symbol token;
+    symbol token1;
+    strcpy(token.name,sym.name);
+    get_sym();///等号
+    token1 = get_sym();//常量的值
+    strcpy(token.type,token1.type);
+    token.value = token1.value;//对字符，将其值保存在value中，读取的token名称是单个字符组成的字符
+    token.level = depth;///层次为当前层
+    token.addr = addr;
+    vm[addr] = token.value;
+    addr = addr + 1;
+    syms[num_i] = token;
+    num_i = num_i + 1;///登入符号表
 }
 void var_dec(symbol sym){
+    symbol token;
+    symbol token1;
+    int size = 1;
+    int i = 0;
+    strcpy(token.name,sym.name);
+    get_sym();//冒号
+    token1 = get_sym();
+    if(strcmp(token1.name,"array")==0){
+        get_sym();///[
+        token1 = get_sym();
+        if(strcmp(token1.type,"integer")==0){
+            size = (int)token1.value;///数组大小
+        }
+        else{
+            error(num_l,11);
+        }
+        get_sym();//]
+        get_sym();//of
+        token1 = get_sym();////这里可能有很多的错误类型，都归类与error11
+    }
 
+    strcpy(token.type,token1.name);///数据类型
+    token.level = depth;
+    token.addr = addr;
+    for(i=0;i<size;i++){
+        vm[addr] = 0;
+        addr ++;
+    }///初始化
+    syms[num_i] = token;
+    num_i = num_i + 1;//登入符号表
 }
 void pro_dec(symbol sym){
 
